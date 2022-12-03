67ef52c28237f1a547310ea0b3af430e2f685d0e
Murilo de Souza Lopes <murilosl@gmail.com>
2022-12-03 19:27:13 -0300

    init

---
+++ WebSocket/WebSocket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebSocket
+//
+//  Created by Murilo de Souza Lopes on 03/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
